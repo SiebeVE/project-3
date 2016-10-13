3694c03e302349279a7c0487b1f5c89983f13efc
vagrant <vagrant@homestead>
2016-10-13 08:40:26 +0000

    added migrations and designs in public

---
+++ database/migrations/2016_10_13_075023_create_books_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->bigInteger('isbn');
+            $table->string('image');
+            $table->text('description');
+            $table->text('author');
+            $table->text('condition'); // mogeljkheden bieden
+            $table->integer('status');
+            $table->integer('owner_id');
+            $table->timestamps();
+
+            $table->foreign('owner_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
---
+++ database/migrations/2016_10_13_080403_update_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->string('firstname');
+            $table->string('lastname');
+
+            $table->string('street');
+            $table->integer('number');
+            $table->integer('postal');
+            $table->string('city');
+            $table->string('country');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
