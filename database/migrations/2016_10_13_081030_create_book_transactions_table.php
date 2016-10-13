d9e04b40c56a6d462ed7651feaa8a9c10f53fc4f
Alex Vanderbist <alex.vanderbist@gmail.com>
2016-10-13 08:40:26 +0000

    added migrations and designs in public

---
+++ database/migrations/2016_10_13_081030_create_book_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('book_id');
+            $table->integer('from_id');
+            $table->integer('to_id');
+            $table->timestamps();
+
+            $table->foreign('from_id')->references('id')->on('users');
+            $table->foreign('to_id')->references('id')->on('users');
+            $table->foreign('book_id')->references('id')->on('books');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_transactions');
+    }
+}
