424341ba7cbd75cc42fa0f9a7033b6790355d43e
Alex Vanderbist <alex.vanderbist@gmail.com>
2016-11-05 23:23:52 +0100

    Updated book detail view

--- resources/views/book/view.blade.php
+++ resources/views/book/view.blade.php
@@ -12,7 +12,7 @@
         <div class="panel panel-book">
             <div class="panel-body">
                 <h1>{{$book->title}}</h1>
-                <h2>by {{$book->author}}</h2>
+                <h2>by {{$book->author ?: 'unknown author'}}</h2>
 
                 <div class="row">
                     <div class="col-sm-4">
@@ -21,29 +21,46 @@
                             <ul class="list-unstyled">
                                 <li>
                                     <i class="fa fa-barcode" aria-hidden="true"></i>
-                                    ISBN: <small>{{$book->isbn}}</small>
+                                    ISBN:
+                                    <small>{{$book->isbn}}</small>
                                 </li>
-                                <li>
-                                    <i class="fa fa-book" aria-hidden="true"></i>
-                                    {{$book->pageCount}} pages
-                                </li>
-                                <li>
-                                    <i class="fa fa-globe" aria-hidden="true"></i>
-                                    {{$book->fullLanguage}}
-                                </li>
+                                @if($book->pageCount)
+                                    <li>
+                                        <i class="fa fa-book" aria-hidden="true"></i>
+                                        {{$book->pageCount}} pages
+                                    </li>
+                                @endif
+                                @if($book->language)
+                                    <li>
+                                        <i class="fa fa-globe" aria-hidden="true"></i>
+                                        {{$book->fullLanguage}}
+                                    </li>
+                                @endif
                             </ul>
                         </div>
                     </div>
+
+
                     <div class="col-sm-8">
                         <div class="panel panel-default">
-                            <div class="panel-heading">
+                            <div class="panel-heading clickable" data-toggle="collapse" data-target="#owners">
                                 <h3 class="panel-title">
-                                    7 available
+                                    <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                                    <i class="fa fa-chevron-down" aria-hidden="true"></i>
+                                    {{$book->owners->count()}} available
                                 </h3>
                             </div>
-                            <table class="table">
-
-                            </table>
+                            <div class="collapse" id="owners">
+                                <table class="table">
+                                    @foreach($book->owners as $owner)
+                                        <tr>
+                                            <td>{{$owner->pivot->condition}}</td>
+                                            <td><i class="fa fa-bicycle" aria-hidden="true"></i> x minutes</td>
+                                            <td>&euro; 5,00</td>
+                                        </tr>
+                                    @endforeach
+                                </table>
+                            </div>
                         </div>
 
                         <div class="description">
@@ -51,6 +68,10 @@
                                 {!!$book->description!!}
                             </p>
                         </div>
+
+                        @if(! $book->description)
+                            <div class="well">No description available...</div>
+                        @endif
                     </div>
                 </div>
 
@@ -61,9 +82,9 @@
 
     <script src="/js/libs/readmore.min.js"></script>
     <script>
-        $(document).ready(function(){
+        $(document).ready(function () {
 
-            (function($) {
+            (function ($) {
 
                 $('.description').readmore();
 
