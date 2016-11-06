a410778c1b229566c8c3cab3fa6cc5e2532f0dc5
Alex Vanderbist <alex.vanderbist@gmail.com>
2016-11-06 23:45:04 +0100

    fixed order in routes file

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Auth::routes();
 Route::get('/notifications', 'HomeController@getNotifications');
 
 // User books routes
+Route::get('/book/add/new', 'BookController@getAddNew');
 Route::get('/book/add', 'BookController@getAdd')->name('book.add');
 Route::get('/book/edit', 'BookController@edit')->name('book.edit');
 Route::get('/book/remove', 'BookController@remove')->name('book.remove');
@@ -27,7 +28,6 @@ Route::get('/my-books', 'BookController@index')->name('book.index');
 Route::get('/book/add/{bookId}', 'BookController@getAddDetail');
 Route::post('/book/add/{bookId}', 'BookController@postAddDetail');
 Route::get('/book/add/finish', 'BookController@getAddFinish');
-Route::get('/book/add/new', 'BookController@getAddNew');
 
 Route::get('/book/transaction/{transaction}', 'BookController@getTransaction')->name('book.transaction');
 
