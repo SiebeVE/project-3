e20113c9a02c0564047230fc1ccac2a7af9b0846
Alex Vanderbist <alex.vanderbist@gmail.com>
2016-11-06 22:35:13 +0100

    fixed routes (I think)

--- routes/web.php
+++ routes/web.php
@@ -19,27 +19,27 @@ Route::get('/', function() {
 Auth::routes();
 
 
-// Library routes
-Route::get('/book/find', 'BookController@getFind')->name('book.find');
-Route::get('/book/{book}', 'BookController@view')->name('book.view');
-Route::get('library', 'LibraryController@index')->name('library');
-
-
-// Book transaction routes
-Route::get('/book/{type}/{bookUser}', 'BookController@getBuyOrBorrow')->name('book.buyorborrow');
-Route::get('/book/{type}/confirm/{transaction}', 'BookController@getConfirmRecieved');
-Route::get('/book/borrow/confirm/giveBack/{transaction}', 'BookController@getConfirmGiveBack');
-
 
 // User books routes
+Route::get('/book/add', 'BookController@getAdd')->name('book.add');
 Route::get('/book/edit', 'BookController@edit')->name('book.edit');
 Route::get('/book/remove', 'BookController@remove')->name('book.remove');
 Route::get('/my-books', 'BookController@index')->name('book.index');
-Route::get('/book/add', 'BookController@getAdd')->name('book.add');
 Route::get('/book/add/{bookId}', 'BookController@getAddDetail');
 Route::post('/book/add/{bookId}', 'BookController@postAddDetail');
 Route::get('/book/add/finish', 'BookController@getAddFinish');
 Route::get('/book/add/new', 'BookController@getAddNew');
+
+
+// Book transaction routes
+Route::get('/book/transaction/{type}/{bookUser}', 'BookController@getBuyOrBorrow')->name('book.buyorborrow');
+Route::get('/book/transaction/{type}/confirm/{transaction}', 'BookController@getConfirmRecieved');
+Route::get('/book/transaction/confirm/giveBack/{transaction}', 'BookController@getConfirmGiveBack');
+
+// Library routes
+Route::get('/book/find', 'BookController@getFind')->name('book.find');
+Route::get('/book/{book}', 'BookController@view')->name('book.view');
+Route::get('library', 'LibraryController@index')->name('library');
 
 
 // Profile routes
