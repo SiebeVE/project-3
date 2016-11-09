39d7e0927719bd62750777411568db5c716bdc6e
Alex Vanderbist <alex.vanderbist@gmail.com>
2016-11-09 23:33:19 +0100

    Changed links and added checkprofilecompletion middleware to all routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,6 +30,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // Always check profile completion
+            'check.profile.completion',
         ],
 
         'api' => [
@@ -52,5 +54,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'check.profile.completion' => \App\Http\Middleware\CheckProfileCompletion::class
     ];
 }
